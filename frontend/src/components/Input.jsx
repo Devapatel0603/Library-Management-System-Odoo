f86ac69da63a5ee780b5a66b41cd83689fd5f10d
divyakumar patel <d.b.patel691@gmail.com>
2024-07-14 09:52:02 +0530

    input created

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-const Input = () => {
-  return <div>Input</div>;
+const Input = ({ name = "Username", type = "text", onChange }) => {
+  return (
+    <div>
+      <label htmlFor={name}>{name.capitalize()}</label>
+      <input
+        type={type}
+        name={name}
+        onChange={onChange}
+        className="w-full p-2.5 rounded-md outline-none"
+      />
+    </div>
+  );
 };
 
 export default Input;
